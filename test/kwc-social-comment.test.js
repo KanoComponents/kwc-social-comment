8178965e5c569f0b0b61f00ab4f6efa5d3256984
Paul Varache <paul@kano.me>
2018-11-12 16:11:17 +0000

    Deprecate

--- test/kwc-social-comment.test.js
+++
@@ -1,8 +0,0 @@
-import '../kwc-social-comments.js';
-
-suite('kwc-social-comments', () => {
-    test('instantiating the element works', () => {
-        const element = document.createElement('kwc-social-comments');
-        assert.equal(element.is, 'kwc-social-comments');
-    });
-});
